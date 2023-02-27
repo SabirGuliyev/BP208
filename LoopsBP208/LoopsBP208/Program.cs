36edbc2b70e499ab25caee90ba7361ba87751ea4
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-02-27 20:53:22 +0400

    Loops classworks added

---
+++ LoopsBP208/LoopsBP208/Program.cs
@@ -0,0 +1,70 @@
+﻿//Task 1
+//int num = 90;
+
+//for (int i = 0; i < num; i++)
+//{
+//    if (i==0)
+//    {
+//        continue;
+//    }
+//    if (i%2==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+
+
+//int i = 0;
+//do
+//{
+//    Console.WriteLine("Hello world");
+//    i++;
+//} while (i<5);
+
+
+
+//string name = "yes";
+
+//while (name=="yes")
+//{
+//    Console.WriteLine("Do you need to delete?");
+//    name=Console.ReadLine();
+
+
+//}
+
+
+//int i = 0;
+//while (i < 10)
+//{
+
+//    i++;
+//    Console.WriteLine(i);
+
+//}
+
+
+
+
+
+
+
+// For
+
+//for (int i = 1; i <= 50; i++)
+//{
+
+//    if (i%20==0)
+//    {
+//        Console.WriteLine(i);
+//        break;
+        
+//    }
+    
+//}
+
+
+
+
