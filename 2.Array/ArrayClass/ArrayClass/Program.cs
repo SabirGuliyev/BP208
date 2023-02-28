f17bcccff5f0a900ab35598d6a846bea10466d1f
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-02-28 21:36:16 +0400

    Array Classwork added

---
+++ 2.Array/ArrayClass/ArrayClass/Program.cs
@@ -0,0 +1,101 @@
+﻿//Big O
+
+//int age = 80;
+//if (age >= 18)
+//{
+//    Console.WriteLine("Yetkindir");
+//}
+//else
+//{
+//    Console.WriteLine("Ushaqsan");
+//}
+
+//int step = 0;
+//for (int i = 0; i < age; i++)
+//{
+//    step++;
+//}
+//Console.WriteLine(step);
+
+//int num = 113;
+//int step = 0;
+//for (int i = 2; i*i < num; i++)
+//{
+//    step++;
+//    if (num%i==0)
+//    {
+//        Console.WriteLine("Murekkeb ededdir");
+//        break;
+//    }
+//}
+
+//Console.WriteLine(step);
+
+
+
+//int num1 =33;
+//int step = 0;
+//if (true)
+//{
+
+//}
+//for (int i = 0; i < num1; i++)
+//{
+
+//    for (int j = 0; j < num1; j++)
+//    {
+//       step++;
+
+//    }
+//}
+
+//Console.WriteLine(step);
+//string name = "Aide";
+
+//string[] students = { "Gunay", "Shahbaz", "Reqsane", name };
+//Console.WriteLine(students[1][5]);
+
+//students[0] = "Namiq";
+//Console.WriteLine(students[students.Length - 1]);
+//for (int i = 0; i <students.Length; i++)
+//{
+//    students[i] += " BP208";
+//    Console.WriteLine(students[i]);
+//}
+
+
+int[,] numbers = { { 77, 32, 33 }, { 45, 12, 45 } };
+object person=new object();
+
+//string name = "aide";
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+//Console.WriteLine(numbers[1,1]);
+
+
+
+//Task 1
+//string name = "Aide";
+//bool result = false;
+//for (int i = 0; i < name.Length; i++)
+//{
+//    if (name[i] == 'a' || name[i]=='A')
+//    {
+//      result= true;
+//        Console.WriteLine("a var");
+//       break;
+//    }
+
+//}
+
+//if (result)
+//{
+//    Console.WriteLine("a var");
+//}
+//else
+//{
+//    Console.WriteLine("A yoxdur");
+//}
