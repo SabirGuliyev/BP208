ed7ecd8184b4601b34c910f66bb241c591a06b8e
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-01 21:14:25 +0400

    Searching and Sorting algorithms added

---
+++ 3.Searching,Sorting/SearchSorting/SearchSorting/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+#region Linear Search
+
+//int[] arr = { 14, 3, 5, 4, 22, 100, 1, 20, 30 };
+
+
+//int search = 30;
+//int step = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    step++;
+//    if (arr[i] == search)
+//    {
+//        Console.WriteLine(arr[i]);
+//        break;
+//    }
+//}
+//Console.WriteLine(arr.Length); 
+#endregion
+
+
+#region Buuble Sort
+
+//int[] numbers = { 11, 3, 7, 5, 34, 25, 66, 51 };
+//int save = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    for (int j = 0; j < numbers.Length; j++)
+//    {
+
+//        if (numbers[i] < numbers[j])
+//        {
+//            save = numbers[i];
+//            numbers[i] = numbers[j];
+//            numbers[j] = save;
+
+//            //numbers[i] += numbers[j];
+//            //numbers[j] = numbers[i] - numbers[j];
+//            //numbers[i] -= numbers[j];
+//        }
+//    }
+//}
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+#endregion
+
+
+#region AB Deyishmek
+//int A = 5;
+//int B = 7;
+
+//A=A+B;
+//B = A-B;
+//A=A-B;
+
+//Console.WriteLine(A+" "+B); 
+#endregion
