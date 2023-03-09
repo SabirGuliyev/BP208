439c6cdcfcb1a61d8d9b1e88649174a99f7f50c6
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-09 21:24:24 +0400

    Js Dom,Functions, Objects added

---
+++ 6..JS Intro,Function,Object, Dom/assets/js/main.js
@@ -0,0 +1,106 @@
+//literal object
+// const person={
+//     name:"Shahbaz",
+//     surname:"Eliyev"
+// }
+// person.name="Namiq"
+// // console.log(person)
+
+// //object
+// var person2=new Object({
+//     name:"Afsana",
+//     surname:"Veliyeva"
+// })
+
+// // //function object
+
+// function Person(first,second ){
+//     this.name=first
+//     this.surname=second
+// }
+// var person3=new Person("Aide","Ekberli")
+
+// // console.log(person3)
+
+
+// class Person{
+ 
+//     constructor(name,surname){
+//         this.Name=name
+//         this.Surname=surname
+//     }
+// }
+
+// const people=[person,person2,person3]
+// console.log(people)
+
+
+// function SayHello(){
+    
+//     console.log("Salam gencler")
+// }
+// SayHello()
+
+// function Calculate(num,num2){
+//     num+=num2
+//     console.log(num)
+// }
+// Calculate(5,12)
+
+// var Calculate=function(num,num2,num3){
+
+//     return num+num2+num3
+// }
+
+// var result=Calculate(5,10,20)
+// console.log(result)
+
+// var Sum=(num,num2)=>num+num2
+// console.log(Sum(5,10))
+
+// var SumExtend=function(num,num2){
+//     return num+num2
+// }
+
+var div=document.getElementById("el1")
+// var divByClass=document.getElementsByClassName("element")[0]
+// var divQuery=document.querySelectorAll("div")
+// div.innerHTML="<p>Salam  <strong>dunya</strong> salam <em> BP208</em></p>"
+
+// var pElement=document.createElement("p")
+// pElement.innerText="Salam"
+// var text=document.createTextNode("Salam")
+
+// pElement.classList.add("red-colored")
+// pElement.appendChild(text)
+// div.appendChild(pElement)
+// div.style.backgroundColor="brown"
+
+
+// const people=
+// [
+//     {
+//         name:"Shahbaz",
+//         surname:"Eliyev"
+//     },
+//     {
+//         name:"Aide",
+//         surname:"Veliyeva"
+//     },
+//     {
+//         name:"Zibeyda",
+//         surname:"Ramazanli"
+//     }
+// ]
+
+// for(let i=0;i<people.length;i++){
+//     var pElement=document.createElement("p")
+//     pElement.innerText=`${people[i].name} ${people[i].surname} `
+
+//     div.appendChild(pElement)
+// }
+
+// console.log(`salam ${people[1].name}`)
+// pElement.classList.remove("red-colored")
+
+
