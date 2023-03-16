1332bf3d07106d30dd2c05f25eb5a0e8c5c63588
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-16 21:20:37 +0400

    Float Flexbox added

--- 8.Movie/assets/js/main.js
+++ 8.Movie/assets/js/main.js
@@ -1,4 +1,33 @@
 let row = document.querySelector('.row')
+let input=document.getElementsByClassName("cus-input")[0]
+let btn=document.getElementsByClassName("cus-btn")[0]
+
+btn.onclick=function(){
+  
+  $.ajax({
+    method:"GET",
+    url:"https://api.tvmaze.com/search/shows?q="+input.value
+  }).done((data)=>{
+    row.innerHTML=""
+    data.forEach(element =>{
+      row.innerHTML+=`<div class="col-4 py-3"  >
+      <div class="card" style="width: 18rem;">
+        <img src="${element.show.image?.medium}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"><b>Name</b>:${element.show.name}</h5>
+          <p class="card-text"><b>Language</b>:${element.show.language}</p>
+          <p class="card-text"><b>Genres</b>:${element.show.genres}</p>
+          <p class="card-text"><b>Imdb</b>:$</p>
+          <a href="details.html?id=${element.show.id}" class="btn btn-primary">Start waching</a>
+        </div>
+      </div>
+    </div>`
+  })
+  
+})
+}
+
+
 
 // $.ajax({
 //   method:"GET",
